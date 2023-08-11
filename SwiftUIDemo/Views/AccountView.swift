21e2248983afd60f08424a4ea4b09bd78aab2076
Quentin Zang <quentinzang@outlook.com>
2023-08-11 16:40:11 +0800

    States和Swipe Actions的使用

--- SwiftUIDemo/Views/AccountView.swift
+++ SwiftUIDemo/Views/AccountView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct AccountView: View {
+    @State var isDeleted = false
+    
     var body: some View {
         NavigationView {
             List {
@@ -55,12 +57,24 @@ struct AccountView: View {
                 .listRowSeparatorTint(.blue)
                 .listRowSeparator(.hidden)
                 Section {
-                    Link(destination: URL(string: "https://apple.com")!) {
-                        HStack {
-                            Label("website", systemImage: "house")
-                            Spacer()
-                            Image(systemName: "link")
-                                .foregroundColor(.secondary)
+                    if !isDeleted {
+                        Link(destination: URL(string: "https://apple.com")!) {
+                            HStack {
+                                Label("website", systemImage: "house")
+                                Spacer()
+                                Image(systemName: "link")
+                                    .foregroundColor(.secondary)
+                            }
+                        }
+                        .swipeActions(edge: .leading, allowsFullSwipe: true) {
+                            Button(action: {isDeleted = true }) {
+                                Label("Delete", systemImage: "trash")
+                            }
+                            .tint(.red)
+                            Button {} label: {
+                                Label("Pin", systemImage: "pin")
+                            }
+                            .tint(.yellow)
                         }
                     }
                     Link(destination: URL(string: "https://youku.com")!) {
