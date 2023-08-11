15f74eb407a4c0fd471e5bbacc2583fc0a0ae828
Quentin Zang <quentinzang@outlook.com>
2023-08-11 09:26:44 +0800

    List的使用

---
+++ SwiftUIDemo/Views/AccountView.swift
@@ -0,0 +1,32 @@
+//
+//  AccountView.swift
+//  SwiftUIDemo
+//
+//  Created by Quentin Zang on 2023/8/11.
+//
+
+import SwiftUI
+
+struct AccountView: View {
+    var body: some View {
+        NavigationView {
+            List {
+                Section {
+                    Text("Settings")
+                    Text("Billing")
+                    Text("Help")
+                }
+                .listRowSeparatorTint(.blue)
+                .listRowSeparator(.hidden)
+            }
+            .listStyle(.insetGrouped)
+            .navigationTitle("Account")
+        }
+    }
+}
+
+struct AccountView_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountView()
+    }
+}
