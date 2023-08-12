b48f7afe781ccc0fd9a6324d78d00897ff94909d
Quentin Zang <quentinzang@outlook.com>
2023-08-12 11:28:41 +0800

    Build a Custom Nav Bar

---
+++ SwiftUIDemo/Navigation/NavigationBar.swift
@@ -0,0 +1,31 @@
+//
+//  NavigationBar.swift
+//  SwiftUIDemo
+//
+//  Created by Quentin Zang on 2023/8/12.
+//
+
+import SwiftUI
+
+struct NavigationBar: View {
+    var title = ""
+    var body: some View {
+        ZStack {
+            Color.clear
+                .background(.ultraThinMaterial)
+                .blur(radius: 10)
+            Text(title)
+                .font(.largeTitle.weight(.bold))
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 20)
+        }
+            .frame(height: 70)
+            .frame(maxHeight: .infinity, alignment: .top)
+    }
+}
+
+struct NavigationBar_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationBar(title: "Featured")
+    }
+}
