0c9db5e900192c4c43cfdeae6c243b4080627ea3
Quentin Zang <quentinzang@outlook.com>
2023-08-12 13:42:32 +0800

    Detect Scroll with Coordinate Space

---
+++ SwiftUIDemo/Model/PreferenceKeys.swift
@@ -0,0 +1,15 @@
+//
+//  PreferenceKeys.swift
+//  SwiftUIDemo
+//
+//  Created by Quentin Zang on 2023/8/12.
+//
+
+import SwiftUI
+
+struct ScrollPreferenceKey: PreferenceKey {
+    static var defaultValue: CGFloat = 0
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+        value = nextValue()
+    }
+}
