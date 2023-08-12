ddc1465b349167e20e25ba2b73b4f85b681ff27c
Quentin Zang <quentinzang@outlook.com>
2023-08-12 14:11:03 +0800

    Animatable Font的使用

---
+++ SwiftUIDemo/Styles/AnimatableFontModifier.swift
@@ -0,0 +1,32 @@
+//
+//  AnimatableFontModifier.swift
+//  SwiftUIDemo
+//
+//  Created by Quentin Zang on 2023/8/12.
+//
+
+import SwiftUI
+
+struct AnimatableFontModifier: AnimatableModifier {
+    var size: Double
+    var weight: Font.Weight = .regular
+    var design: Font.Design = .default
+    
+    var animatableData: Double {
+        get { size }
+        set { size = newValue }
+    }
+    
+    func body(content: Content) -> some View {
+        content
+            .font(.system(size: size, weight: weight, design: design))
+    }
+}
+
+extension View {
+    func animatableFont(size: Double, weight: Font.Weight = .regular, design: Font.Design = .default) -> some View {
+        self.modifier(AnimatableFontModifier(size: size, weight: weight, design: design))
+    }
+}
+
+
