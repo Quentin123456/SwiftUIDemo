217a89c10043d5f6202c7d8629bb906d1d46a01f
Quentin Zang <quentinzang@outlook.com>
2023-08-14 16:03:37 +0800

    Astnc__Await

---
+++ SwiftUIDemo/Model/Address.swift
@@ -0,0 +1,13 @@
+//
+//  Address.swift
+//  SwiftUIDemo
+//
+//  Created by Quentin Zang on 2023/8/14.
+//
+
+import SwiftUI
+
+struct Address: Identifiable, Decodable {
+    var id: Int
+    var country: String
+}
