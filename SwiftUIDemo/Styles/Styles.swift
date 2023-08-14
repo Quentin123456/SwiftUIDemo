bd8b025ec551899de44ded8fdf9991da25c6b58c
Quentin Zang <quentinzang@outlook.com>
2023-08-14 17:31:05 +0800

    Custom Progress View

--- SwiftUIDemo/Styles/Styles.swift
+++ SwiftUIDemo/Styles/Styles.swift
@@ -7,9 +7,10 @@
 
 import SwiftUI
 
-struct StrokeStyle: ViewModifier {
+struct StrokeModifier: ViewModifier {
     var cornerRadius: CGFloat
     @Environment(\.colorScheme) var colorScheme
+    
     func body(content: Content) -> some View {
         content.overlay(
             RoundedRectangle(cornerRadius: cornerRadius, style: .continuous)
@@ -17,15 +18,17 @@ struct StrokeStyle: ViewModifier {
                     .linearGradient(
                         colors: [
                             .white.opacity(colorScheme == .dark ? 0.1 : 0.3),
-                            .black.opacity(colorScheme == .dark ? 0.3 : 0.1)], startPoint: .top, endPoint: .bottom))
+                            .black.opacity(colorScheme == .dark ? 0.3 : 0.1)
+                        ], startPoint: .top, endPoint: .bottom
+                    )
+                )
                 .blendMode(.overlay)
         )
     }
 }
 
-
 extension View {
     func strokeStyle(cornerRadius: CGFloat = 30) -> some View {
-        modifier(StrokeStyle(cornerRadius: cornerRadius))
+        modifier(StrokeModifier(cornerRadius: cornerRadius))
     }
 }
