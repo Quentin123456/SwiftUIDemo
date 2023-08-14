6cefd79126281a462c969ceb13462b27f0fb5e07
Quentin Zang <quentinzang@outlook.com>
2023-08-14 17:47:10 +0800

    Play Button

--- SwiftUIDemo/Views/Main/ExploreView.swift
+++ SwiftUIDemo/Views/Main/ExploreView.swift
@@ -7,14 +7,70 @@
 
 import SwiftUI
 
-struct ExporeView: View {
+struct ExploreView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color("Background").ignoresSafeArea()
+            
+            
+            ScrollView {
+                coursesSection
+                
+//                Text("Topics".uppercased())
+//                    .titleStyle()
+                
+//                topicsSection
+                
+//                Text("Popular".uppercased())
+//                    .titleStyle()
+                
+//                handbooksSection
+            }
+            
+            .safeAreaInset(edge: .top) {
+                Color.clear.frame(height: 70)
+            }
+            .overlay(NavigationBar(title: "Recent", hasScrolled: .constant(true)))
+            .background(Image("Blob 1").offset(x: -100, y: -400))
+        }
     }
+    
+    var coursesSection: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 16) {
+                ForEach(courses) { course in
+                    SmallCourseItem(course: course)
+                }
+            }
+            .padding(.horizontal, 20)
+            Spacer()
+        }
+    }
+    
+//    var handbooksSection: some View {
+//        HStack(alignment: .top, spacing: 16) {
+//            ForEach(handbooks) { handbook in
+//                HandbookItem(handbook: handbook)
+//            }
+//        }
+//        .padding(.horizontal, 20)
+//    }
+//
+//    var topicsSection: some View {
+//        VStack {
+//            ForEach(topics) { topic in
+//                ListRow(topic: topic)
+//            }
+//        }
+//        .padding(20)
+//        .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30, style: .continuous))
+//        .strokeStyle(cornerRadius: 30)
+//        .padding(.horizontal, 20)
+//    }
 }
 
-struct ExporeView_Previews: PreviewProvider {
+struct ExploreView_Previews: PreviewProvider {
     static var previews: some View {
-        ExporeView()
+        ExploreView()
     }
 }
